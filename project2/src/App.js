c835a007a07672c1d9cb4ca3ec3f272a27a29e50
Yash <yash38tomar22@gmail.com>
2022-07-08 00:48:25 +0530

    last comit for the day

---
+++ project2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavigationBar from './components/Navigationbar';
+import Firstbar from './components/Firstbar';
+
+function App() {
+  return (
+    <div className="App">
+     <NavigationBar/>
+     <Firstbar/>
+    </div>
+  );
+}
+
+export default App;
